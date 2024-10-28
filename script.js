e68144ebe0521894950d46a3a9fc59b216fb4113
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-10-28 15:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+   const heap = [...arr];
   
+  const heapify = () => heap.sort((a, b) => a - b);
+
+  let totalCost = 0;
+  
+  heapify();
+  
+  while (heap.length > 1) {
+    const first = heap.shift();
+    const second = heap.shift();
+    
+    const newRope = first + second;
+    totalCost += newRope;
+    
+    heap.push(newRope);
+    heapify();
+  }
+  
+  return totalCost;
 }
 
 module.exports=mincost;
